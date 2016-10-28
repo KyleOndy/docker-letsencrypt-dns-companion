a6aa400f59515990d4ff844b574639fe0cf58647
Kyle Ondy <kyle@ondy.me>
2016-10-28 15:22:49 -0400

    Tidy up script

--- dns-certbot.sh
+++ dns-certbot.sh
@@ -6,11 +6,7 @@ OUTPUT_LOCATION="${CERTBOT_OUTPUT:-/certs}"
 while :;
 do
   DEHYDRATED_DIR=/dehydrated
-  echo "Running dehydrated"
-
   "$DEHYDRATED_DIR/dehydrated" --cron --hook "$DEHYDRATED_DIR/dehydrated.default.sh" --challenge dns-01 --domain "$CERTBOT_DOMAIN" --out "$OUTPUT_LOCATION"
-
-
   echo "Sleeping for $SLEEP_TIME until next check."
   sleep "$SLEEP_TIME"
 done
